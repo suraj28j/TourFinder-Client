2b4a7d7efcd43f4ef82f2530032f5f7a586d808a
Suraj Kumar <suraj28j@gmail.com>
2024-11-01 22:39:52 +0530

    Login and Registration add update

--- tour_finder/src/components/Login/Login.jsx
+++ tour_finder/src/components/Login/Login.jsx
@@ -1,14 +1,59 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import loginImg from '../../assets/images/login.png'
 import userImg from '../../assets/images/user.png'
 import './Login.css';
 
 import { Link } from 'react-router-dom'
+import { BASE_URL } from '../../utilis/config';
+import { AuthContext } from '../../context/AuthContext';
 
 
 const Login = () => {
+
+  const { dispatch } = useContext(AuthContext)
+
+  const [credentials, setCredentials] = useState({
+    email: undefined,
+    password: undefined
+  })
+
+  const handleChange = (e) => {
+    setCredentials((preVal) => ({ ...preVal, [e.target.id]: e.target.value }));
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await fetch(`${BASE_URL}/auth/loginuser`, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(credentials)
+      })
+      const result = await res.json();
+      console.log(result);
+
+      result.success === true ? alert("Login Successful") : alert("Login Failed");
+
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: result.data,
+        token: result.token,
+        role: result.role
+      })
+
+    } catch (error) {
+      dispatch(
+        {
+          type: "LOGIN_FAILURE",
+          payload: error.message
+        })
+    }
+
+  }
+
   return (
-    <div className='container' style={{marginTop:'5%'}}>
+    <div className='container' style={{ marginTop: '5%' }}>
       <div className='row'>
         <div className='col-md-3'></div>
         <div className='col-md-6'>
@@ -19,11 +64,11 @@ const Login = () => {
             </div>
             <div className='col-md-5 bg-warning'>
               <div className='ms-4 me-4 text-center mt-4'>
-                <img src={userImg} alt='userImg' className='w-25 userImg'/>
+                <img src={userImg} alt='userImg' className='w-25 userImg' />
                 <h3 className='mt-4'>Login</h3>
-                <form>
-                  <input type='email' className='form-control mt-4' placeholder='Email' />
-                  <input type='password' className='form-control mt-3' placeholder='Password' />
+                <form onSubmit={handleClick}>
+                  <input type='email' id='email' className='form-control mt-4' placeholder='Email' onChange={handleChange} />
+                  <input type='password' id='password' className='form-control mt-3' placeholder='Password' onChange={handleChange} />
                   <button className='btn btn-dark mt-3 w-100'>Login</button>
                   <p className='mt-4'>Don't have an account? <Link to='/register' className='nav-link text-dark'>Register</Link></p>
                 </form>
--- tour_finder/src/components/Register/Register.jsx
+++ tour_finder/src/components/Register/Register.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import loginImg from '../../assets/images/login.png'
 import userImg from '../../assets/images/user.png'
 import { Link } from 'react-router-dom'
+import { BASE_URL } from '../../utilis/config'
 
 const Register = () => {
+  const [credentials, setCredentials] = useState({
+    name: undefined,
+    email: undefined,
+    password: undefined
+  })
+
+  const handleChange = (e) => {
+    setCredentials((preVal) => ({ ...preVal, [e.target.id]: e.target.value }))
+  }
+
+  // console.log(credentials);
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`${BASE_URL}/auth/registeruser`, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(credentials)
+      })
+      const result = await res.json();
+
+      if (!result.ok) {
+        console.log("Error Mesasge : ", result.message);
+      }
+      if (result.success === true) {
+        alert("Registration Successful")
+      } else {
+        alert("Registration Faild")
+      }
+    } catch (error) {
+      console.log("Internal Error", error);
+    }
+  }
+
   return (
-    <div className='container ' style={{marginTop:'5%'}}>
+    <div className='container' style={{ marginTop: '5%' }}>
       <div className='row'>
         <div className='col-md-3'></div>
         <div className='col-md-6'>
@@ -16,12 +53,12 @@ const Register = () => {
             </div>
             <div className='col-md-5 bg-warning'>
               <div className='ms-4 me-4 text-center mt-4'>
-                <img src={userImg} alt='userImg' className='w-25 userImg'/>
+                <img src={userImg} alt='userImg' className='w-25 userImg' />
                 <h3 className='mt-4'>Register</h3>
-                <form>
-                  <input type='text' className='form-control mt-4' placeholder='User Name' />
-                  <input type='email' className='form-control mt-3' placeholder='Email' />
-                  <input type='password' className='form-control mt-3' placeholder='Password' />
+                <form onSubmit={handleClick}>
+                  <input type='text' id='name' className='form-control mt-4' placeholder='User Name' onChange={handleChange} />
+                  <input type='email' id='email' className='form-control mt-3' placeholder='Email' onChange={handleChange} />
+                  <input type='password' id='password' className='form-control mt-3' placeholder='Password' onChange={handleChange} />
                   <button className='btn btn-dark mt-3 w-100'>Creat Account</button>
                   <p className='mt-4'>Already have an account? <Link to='/login' className='nav-link text-dark'>Login</Link></p>
                 </form>
