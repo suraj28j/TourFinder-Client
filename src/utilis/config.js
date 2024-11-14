963ca5ba11d26b8607a91f7705dc1714b72142d1
Suraj Kumar <suraj28j@gmail.com>
2024-11-14 13:01:51 +0530

    BASE URL Change

--- src/utilis/config.js
+++ src/utilis/config.js
@@ -1,3 +1,5 @@
-export const BASE_URL = "http://localhost:8000/api/v1";
+// export const BASE_URL = "http://localhost:8000/api/v1";
+export const BASE_URL = "https://tourfinder.onrender.com/api/v1";
+
 
 export const token = localStorage.getItem('token');
